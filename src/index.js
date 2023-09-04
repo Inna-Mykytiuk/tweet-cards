485e819177fcb0d88265c373adb229a356870b13
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-04 19:47:30 +0300

    update

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { store } from 'redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename="/tweet-cards">
         <App />
       </BrowserRouter>
     </Provider>
-  // </React.StrictMode>
+    </React.StrictMode>
 );
