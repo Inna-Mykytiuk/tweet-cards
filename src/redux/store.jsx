cfcafb746ece0831546704ddf11595b9815b415e
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-04 10:50:15 +0300

    update redux store

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
 import { usersReducer } from './usersSlice';
 import { filtersReducer } from './filtersSlice';
 
+const rootReducer = combineReducers({
+  users: usersReducer,
+  filters: filtersReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistTasksReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    users: usersReducer,
-    filters: filtersReducer,
-  },
+  reducer: persistTasksReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
