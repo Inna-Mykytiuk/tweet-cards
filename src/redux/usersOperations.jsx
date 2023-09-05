196e7bb6d87df154998083b661085e25583fb3d9
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-05 20:29:55 +0300

    add

--- src/redux/usersOperations.jsx
+++ src/redux/usersOperations.jsx
@@ -8,6 +8,7 @@ export const fetchUsers = createAsyncThunk(
   async ( _, { rejectWithValue }) => {
     try {
       const response = await axios.get('/users');
+      console.log(response.data)
       return response.data;
     } catch (e) {
       return rejectWithValue(e.message);
